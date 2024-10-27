54ae06cc733ad70ef97107b94941ae2c2b1ee409
Thierry PAGES <thipages@gmail.com>
2024-10-27 12:04:40 +0100

    sub-package correction + define export

--- esm.js
+++ esm.js
@@ -182,3 +182,5 @@ function define(options) {
 }
 
 define({render});
+
+export { define };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import render from "./render/index.js"
 import {define} from './text-m.js'
+export {define}
 define({render})
