e90dd6aac89e10c18dd29a5399f3ad9f47482e3c
Thierry PAGES <thipages@gmail.com>
2024-11-18 06:13:48 +0100

    adapat to new m-element version

--- src/text-m.js
+++ src/text-m.js
@@ -2,11 +2,11 @@ import MElement from '@titsoft/m-element'
 const def = { name: 'text-m' }
 export function define(options) {
     const {name, render} = Object.assign(options, def)
-    customElements.define(
-        name, class extends MElement {
+    customElements.define(name,
+        class extends MElement {
             constructor() { super() }
             init() {
-                this.innerHTML = render(this.textContent)
+                this.innerHTML = render(this.originalText())
             }
         }
     )
